969c753075f629d8e26c4fa53cf60af3c8efd0ad
rushi-0369 <23211a0556@bvrit.ac.in>
2026-04-05 12:46:55 +0530

    Added Backend for Conatct and Hero page animation

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = ({ darkMode, contactRef }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !message) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    const formData = {
+      name,
+      email,
+      message,
+    };
+
+    try {
+      const res = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success) {
+        alert("Message Sent ✅");
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <section
       ref={contactRef}
@@ -26,7 +64,7 @@ const Contact = ({ darkMode, contactRef }) => {
         </div>
         <div className="contact-grid">
           <div className="contact-form-wrapper">
-            <form action="" className="contact-form ">
+            <form action="" className="contact-form" onSubmit={handleSubmit}>
               <div className="form-group">
                 <label htmlFor="" className="form-label">
                   Name
@@ -36,6 +74,8 @@ const Contact = ({ darkMode, contactRef }) => {
                   id="name"
                   className="form-input"
                   placeholder="Your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -43,10 +83,12 @@ const Contact = ({ darkMode, contactRef }) => {
                   Email
                 </label>
                 <input
-                  type="text"
+                  type="email"
                   id="email"
                   className="form-input"
                   placeholder="your@gmail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -59,6 +101,8 @@ const Contact = ({ darkMode, contactRef }) => {
                   id="message"
                   className="form-textarea"
                   placeholder="Your message..."
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
               </div>
               <button type="submit" className="contact-submit-btn">
@@ -104,9 +148,9 @@ const Contact = ({ darkMode, contactRef }) => {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="lucide lucide-github w-5 h-5"
                 >
                   <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
@@ -153,13 +197,13 @@ const Contact = ({ darkMode, contactRef }) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="size-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
                   />
                 </svg>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,28 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+const texts = ["Problem Solver", "Frontend Developer"];
 const Hero = ({ darkMode }) => {
+  const [textIndex, setTextIndex] = useState(0);
+  const [displayText, setDisplayText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  useEffect(() => {
+    const currentText = texts[textIndex];
+    let speed = isDeleting ? 50 : 100;
+    const timeout = setTimeout(() => {
+      if (!isDeleting) {
+        setDisplayText(currentText.substring(0, displayText.length + 1));
+      } else {
+        setDisplayText(currentText.substring(0, displayText.length - 1));
+      }
+      if(!isDeleting && displayText === currentText){
+        setTimeout(()=>setIsDeleting(true), 1200);
+      }
+      if (isDeleting && displayText === "") {
+        setIsDeleting(false);
+        setTextIndex((prev) => (prev + 1) % texts.length);
+      }
+    }, speed);
+    return () => clearTimeout(timeout);
+  }, [displayText, isDeleting, textIndex]);
   return (
     <div>
       <section className={`hero-section ${darkMode ? "dark" : "light"}`}>
@@ -12,7 +34,10 @@ const Hero = ({ darkMode }) => {
 
           <h1 className="hero-name">Rushi</h1>
 
-          <h2 className="hero-role">A Coder</h2>
+          <h2 className="hero-role">
+            {displayText}
+            <span className="cursor-role">|</span>
+          </h2>
 
           <div className="terminal">
             <div className="terminal-header">
